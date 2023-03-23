03c9a681193f37d849445e3ba6737c4e830c5bca
Mel <mellpazzanese@icloud.com>
2023-03-23 19:42:32 +0000

    cards and buttons added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-// This is from activity 3 and 4 HelloBootstrap:
-
-//import React from 'react';
-
-// import Hellobootstrap from './components/HelloBootstrap';
-
-
-// function App(){
-// return <Hellobootstrap/>
-
-// }
-
-// export default App;
-
-//ACTIVITY 05 - JSXVariables
 import React from 'react';
 
-import JSXVariables from './components/JSXVariables';
+import Hellobootstrap from './components/HelloBootstrap';
 
-function App (){
-    return <JSXVariables/>
+
+function App(){
+return <Hellobootstrap/>
+
 }
 
 export default App;
--- src/components/HelloBootstrap.js
+++ src/components/HelloBootstrap.js
@@ -1,72 +1,63 @@
-// import React from 'react';
+import React from 'react';
 
-// function HelloBootstrap(){
-//     return (
-//         <div className="container">
-//             <div className="jumbotron">
-//                 <h1>Hello World</h1>
-//                 <p>I'm a jumbotron</p>
-//                 <p><button className="btn btn-primary">
-//                     Learn more
-//                     </button>
-//                     </p>
-//             </div>
-//         <div className="card mb-4">
-//         <div className="card-header">
-//             <h3>Card title</h3>
-//         </div>
-//         <div className="card-body">
-//             <p className="card-text">card container</p>
+function HelloBootstrap(){
+    return (
+        <div className="container">
+            <div className="jumbotron bg-info class">
+                <h1>Melissa Pazzanese's Portfolio</h1>
+                <p>mellpazzanese@icloud.com</p>
+                <p><button className="btn btn-secondary">
+                    Learn more
+                    </button>
+                    </p>
+            </div>
+        <div className="card mb-4">
+        <div className="card-header bg-info class">
+            <h3>Project 1</h3>
+        </div>
+        <div className="card-body">
+            <p className="card-text">password-generator2</p>
+            <p><button className="btn btn-secondary">
+                    Learn more
+                    </button>
+                    </p>
 
-//         </div>
-//         </div>
+        </div>
+        </div>
 
-//         <div className="card mb-4">
-//         <div className="card-header">
-//             <h3>Card title</h3>
-//         </div>
-//         <div className="card-body">
-//             <p className="card-text">card container</p>
+        <div className="card mb-4">
+        <div className="card-header bg-info class">
+            <h3>Project 2</h3>
+        </div>
+        <div className="card-body">
+            <p className="card-text">console_finances </p>
+            <p><button className="btn btn-secondary">
+                    Learn more
+                    </button>
+                    </p>
 
-//         </div>
-//         </div>
+        </div>
+        </div>
 
-//         <div className="card mb-4">
-//         <div className="card-header">
-//             <h3>Card title</h3>
-//         </div>
-//         <div className="card-body">
-//             <p className="card-text">card container</p>
+        <div className="card mb-4">
+        <div className="card-header bg-info class">
+            <h3>Project 3</h3>
+        </div>
+        <div className="card-body">
+            <p className="card-text">bootstrap-portfolio</p>
+            <p><button className="btn btn-secondary">
+                    Learn more
+                    </button>
+                    </p>
 
-//         </div>
-//         </div>
-//         <div class="alert alert-primary" role="alert">
-//   A simple primary alert—check it out!
-// </div>
-// <div class="alert alert-secondary" role="alert">
-//   A simple secondary alert—check it out!
-// </div>
-// <div class="alert alert-success" role="alert">
-//   A simple success alert—check it out!
-// </div>
-// <div class="alert alert-danger" role="alert">
-//   A simple danger alert—check it out!
-// </div>
-// <div class="alert alert-warning" role="alert">
-//   A simple warning alert—check it out!
-// </div>
-// <div class="alert alert-info" role="alert">
-//   A simple info alert—check it out!
-// </div>
-// <div class="alert alert-light" role="alert">
-//   A simple light alert—check it out!
-// </div>
-// <div class="alert alert-dark" role="alert">
-//   A simple dark alert—check it out!
-// </div>
-//         </div>
-//     )
-// }
+        </div>
+        </div>
+        
 
 
-// export default HelloBootstrap;
+        </div>
+    )
+}
+
+
+export default HelloBootstrap;
