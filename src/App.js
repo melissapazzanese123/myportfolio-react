93becbf43334bceeac27a058cdcbe4e77ee62f4e
Mel <mellpazzanese@icloud.com>
2023-03-23 19:06:06 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+// This is from activity 3 and 4 HelloBootstrap:
+
+//import React from 'react';
+
+// import Hellobootstrap from './components/HelloBootstrap';
+
+
+// function App(){
+// return <Hellobootstrap/>
+
+// }
+
+// export default App;
+
+//ACTIVITY 05 - JSXVariables
+import React from 'react';
+
+import JSXVariables from './components/JSXVariables';
+
+function App (){
+    return <JSXVariables/>
+}
+
+export default App;
---
+++ src/components/JSXVariables.js
@@ -0,0 +1,29 @@
+
+//ACTIVITY 05- JSXVariables
+// import React from 'react';
+
+
+// const name = 'Melissa';
+// const num1 = '1';
+// const num2 = '2';
+
+
+// function JSXVariables (){
+//     return (
+//         <div className= 'main-container'>
+//             <div className='container'>
+//                 <div className='jumbotron'>
+//                 <h2>My name is {name}</h2>
+
+//                 </div>
+
+//             </div>
+
+//         </div>
+//     )
+// }
+
+// export default JSXVariables;
+
+//ACTIVITY 06 - JSXVariables 
+
